a1bb59c85f65f794f1ac42adf278e8bb615006f5
Rachel <rmasey@farnborough.ac.uk>
2020-11-18 21:30:34 +0000

    fixed

--- src/main/java/controllers/Champions.java
+++ src/main/java/controllers/Champions.java
@@ -14,23 +14,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
-@Path("Champions/")
+@Path("champions/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
 public class Champions {
 
     @GET
-    @Path("getHp/{Level}")
-    public String GetHp(@PathParam("Level") Integer Level) {
+    @Path("getHp/{level}")
+    public String GetHp(@PathParam("level") Integer level) {
 
         JSONArray response = new JSONArray();
-        System.out.println("Invoked Champions.getHp() with Level " + Level);
+        System.out.println("Invoked Champions.getHp() with level " + level);
 
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT Hp,CombinedID FROM Stats WHERE CombinedID=(SELECT CombinedID FROM Levels WHERE Level = ?) ORDER BY Hp DESC ");
+            ps.setInt(1, level);
             ResultSet results = ps.executeQuery();
-            while (results.next()==true) {
+            while (results.next() == true) {
                 JSONObject row = new JSONObject();
                 row.put("Hp", results.getInt(1));
                 row.put("CombinedID", results.getInt(2));
