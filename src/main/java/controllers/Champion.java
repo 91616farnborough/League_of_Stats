bc2e78d9224f7d5294b87584f2a27d7d612e8909
91616farnborough <91616@farnborough.ac.uk>
2020-12-17 23:42:08 +0000

    Champions page implemented next needs favourite button and admin page variant page(access the user token or create an authorised user column in the table that can be Null otherwise admin. Find a place to put the favourites dropdown box and a recents dropdown box.

--- src/main/java/controllers/Champion.java
+++ src/main/java/controllers/Champion.java
@@ -42,6 +42,76 @@ public class Champion {
     }
 
     @POST
+    @Path("getall")
+    public String championGetall (@FormDataParam("StatsID") Integer StatsID) {
+        System.out.println("Invoked champion.getStats()"+StatsID);
+        System.out.println("StatsID " + StatsID);
+
+        JSONArray response = new JSONArray();
+
+        String query = "SELECT Hp,Hpr,Mp,Mpr,AD,AP,PR,MR,AttackSpeed,MS,Range,Level,ChampionID FROM Stats WHERE StatsID="+StatsID;  //means order the records in descending order of WordID and take only the first which will have the highest ID value
+        try (Statement stmt = Main.db.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("Hp", results.getString(1));
+                row.put("Hpr", results.getString(2));
+                row.put("Mp", results.getString(3));
+                row.put("Mpr", results.getString(4));
+                row.put("AD", results.getString(5));
+                row.put("AP", results.getString(6));
+                row.put("PR", results.getString(7));
+                row.put("MR", results.getString(8));
+                row.put("AttackSpeed", results.getString(9));
+                row.put("MS", results.getString(10));
+                row.put("Range", results.getString(11));
+                row.put("Level", results.getString(12));
+                row.put("ChampionID", results.getString(13));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("Levelgetall")
+    public String championLevelGetall (@FormDataParam("ChampionID") Integer ChampionID, @FormDataParam("Level") Integer Level) {
+        System.out.println("Invoked champion.getLevelStats()"+ChampionID);
+        System.out.println("ChampionID: " + ChampionID);
+        System.out.println("Level: " + Level);
+
+        JSONArray response = new JSONArray();
+
+        String query = "SELECT Hp,Hpr,Mp,Mpr,AD,AP,PR,MR,AttackSpeed,MS,Range FROM Stats WHERE ChampionID="+ChampionID+" AND Level="+Level;
+        try (Statement stmt = Main.db.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("Hp", results.getString(1));
+                row.put("Hpr", results.getString(2));
+                row.put("Mp", results.getString(3));
+                row.put("Mpr", results.getString(4));
+                row.put("AD", results.getString(5));
+                row.put("AP", results.getString(6));
+                row.put("PR", results.getString(7));
+                row.put("MR", results.getString(8));
+                row.put("AttackSpeed", results.getString(9));
+                row.put("MS", results.getString(10));
+                row.put("Range", results.getString(11));
+                response.add(row);
+            }
+            System.out.println("Database returns: "+response.toString());
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
     @Path("abc")
     public String abcGet (@FormDataParam("level") Integer level) {
         System.out.println("Invoked champion.getStat()");
@@ -58,6 +128,49 @@ public class Champion {
                 response.add(row);
             }
             return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("Getname")
+    public String Getname (@FormDataParam("ChampionID") Integer ChampionID) {
+        System.out.println("Invoked champion.getStat() ID:"+ChampionID);
+
+        JSONArray response = new JSONArray();
+
+        String query ="SELECT Name,ImagePath FROM Champions WHERE ChampionID="+ChampionID;
+        try (Statement stmt = Main.db.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("name", results.getString(1));
+                row.put("imagePath", results.getString(2));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("GetChampID")
+    public String GetChampID (@FormDataParam("StatsID") Integer StatsID) {
+        System.out.println("Invoked champion.getChampID() statsID:"+StatsID);
+
+        JSONArray response = new JSONArray();
+
+        String query ="SELECT ChampionID FROM Stats WHERE StatsID = "+StatsID;
+        try (Statement stmt = Main.db.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            JSONObject row = new JSONObject();
+            row.put("ChampionID", results.getString(1));
+            System.out.println(row.toString());
+            return row.toString();
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
--- src/main/java/controllers/Favourite.java
+++ src/main/java/controllers/Favourite.java
@@ -31,4 +31,21 @@ public class Favourite {
         }
 
     }
+
+    @POST
+    @Path("statsID")
+    public String UserID(@FormDataParam("userID") Integer userID) {
+        System.out.println("Invoked StatsID() on path Pages/statsID "+userID);
+        String query = "SELECT StatsID FROM Favourites WHERE UserID="+userID+" ORDER BY PageID DESC";
+        try (Statement stmt = Main.db.createStatement()) {
+            ResultSet results = stmt.executeQuery(query);
+            JSONObject row = new JSONObject();
+            row.put("StatsID", results.getString(1));
+            System.out.println(row.toString());
+            return row.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items. User Not authorised.\"}";
+        }
+    }
 }
